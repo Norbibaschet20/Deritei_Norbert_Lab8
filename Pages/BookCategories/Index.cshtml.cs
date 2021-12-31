d42fe9b866b6ad9593cd18b2fe11b9da2d715412
norbi.deritei <deriteinorbi@yahoo.com>
2021-12-31 17:51:06 +0200

    Lab9

---
+++ Pages/BookCategories/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Deritei_Norbert_Lab8.Data;
+using Deritei_Norbert_Lab8.Models;
+
+namespace Deritei_Norbert_Lab8.Pages.BookCategories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Deritei_Norbert_Lab8.Data.Deritei_Norbert_Lab8Context _context;
+
+        public IndexModel(Deritei_Norbert_Lab8.Data.Deritei_Norbert_Lab8Context context)
+        {
+            _context = context;
+        }
+
+        public IList<BookCategory> BookCategory { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            BookCategory = await _context.BookCategory
+                .Include(b => b.Book)
+                .Include(b => b.Category).ToListAsync();
+        }
+    }
+}
